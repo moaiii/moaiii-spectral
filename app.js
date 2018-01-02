32bc35fcad983ae5a64bc0e43cc938c0c9131677
moaiii <c.melville@hotmail.co.uk>
2018-01-02 17:55:54 +0000

    Added favicon

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = module.exports.app = exports.app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
